d2b613c5250392f013a4843f58eb1411cf30a8d6
Naoto Kaneko <naoty.k@gmail.com>
2017-02-05 21:46:59 +0900

    Add urfave/cli as dependency

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+// Version is the version of this application.
+var Version = "0.1.0"
+
 func main() {
+	app := cli.NewApp()
+	app.Name = "changelog"
+	app.Version = Version
+	app.Usage = "Manage CHANGELOG in my style"
+	app.Author = "Naoto Kaneko"
+	app.Email = "naoty.k@gmail.com"
+	app.Run(os.Args)
 }
